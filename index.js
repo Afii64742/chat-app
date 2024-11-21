d6db802b3b58b271aa1923c31f8c217e75155739
Aftab64742 <afii64742@gmail.com>
2024-11-21 20:14:40 +0500

    DB connection established and user model is created

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from "express"
 import http from "http"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+dotenv.config();
 const app = express()
+const PORT = process.env.PORT || 8080
 const server = http.createServer(app)
+
+// DB connection 
+connectDB(process.env.MONGO_URI).then(()=>{
+    console.log("DB connected")
+    server.listen(PORT, () => {
+        console.log("Server is running on port 3000")
+    })
+}).catch((err)=>{
+    console.log("DB Connection error", err)
+})
+
+//middlwares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// custom routes 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send("Welcome to chat application!")
 })
-server.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+
